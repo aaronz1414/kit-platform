9b8891452a09be6c178663fe02630e19253514b6
Aaron Zalewski <aaronz1414@gmail.com>
2023-11-18 15:55:15 -0500

    Added progress field to article

--- apps/services/activity/src/main.ts
+++ apps/services/activity/src/main.ts
@@ -3,6 +3,7 @@ import { buildSubgraphSchema } from '@apollo/subgraph';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import * as fs from 'fs';
 import gql from 'graphql-tag';
+import { GraphQLScalarType, Kind } from 'graphql';
 import * as path from 'path';
 
 // Schema
@@ -18,12 +19,35 @@ const typeDefs = gql(
 
 // Resolvers
 // ---------
+const dateTime = new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'Custom scalar type representing an ISO datestring',
+    serialize(value: Date) {
+        return value.toISOString();
+    },
+    parseValue(value: string) {
+        return new Date(value);
+    },
+    parseLiteral(ast) {
+        if (ast.kind === Kind.STRING) {
+            return new Date(ast.value);
+        }
+        return null;
+    },
+});
+
 const resolvers = {
+    DateTime: dateTime,
     Article: {
         __resolveReference(rep) {
             return {
                 id: rep.id,
-                testField: 'I am an article',
+                myProgress: {
+                    startedAt: new Date(),
+                    lastProgressAt: new Date(),
+                    completedAt: new Date(),
+                    percentage: 50,
+                },
             };
         },
     },
